fff1e7abbf00d9e3494840492188fcb73d846be3
Renata Minigalieva <ellyclover@gmail.com>
2023-05-24 18:45:40 -0400

    change window title with ipcRenderer

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
-const { app, BrowserWindow } = require('electron')
 
-const createWindow = () => {
-    const mainWindow = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-      },
-    })
-  
-    mainWindow.loadFile('index.html')
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools()
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  ipcMain.on('set-title', (event, title) => {
+    const webContents = event.sender
+    const win = BrowserWindow.fromWebContents(webContents)
+    win.setTitle(title)
+  })
+
+  mainWindow.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-    createWindow()
+  createWindow()
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 })
 
-// Quit the app when all windows are closed (Windows & Linux)
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
 })
--- renderer.js
+++ renderer.js
@@ -1,3 +1,6 @@
-const information = document.getElementById('info')
-
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+const setButton = document.getElementById('btn')
+const titleInput = document.getElementById('title')
+setButton.addEventListener('click', () => {
+  const title = titleInput.value
+  window.electronAPI.setTitle(title)
+})
